19718ff3242a5675526151dcc3641faf22b3d467
elham-qolizade <168982184+elham-qolizade@users.noreply.github.com>
2024-10-17 18:37:47 +0330

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
-
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -13,11 +13,10 @@ export default function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/menu' element={<Menu />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 import { FaInstagram } from "react-icons/fa";
 import { CiTwitter } from "react-icons/ci";
 import { RiTelegramLine } from "react-icons/ri";
 import { CiLinkedin } from "react-icons/ci";
-
-import '../styles/footer.css'
+import "../styles/footer.css";
 export default function footer() {
-    return (
-        <div className='footer'>
-            <div className='socialMedia'>
-                <FaInstagram />
-                <CiTwitter />
-                <RiTelegramLine />
-                <CiLinkedin />
-
-            </div>
-            <p>&copy: 2021pedrotechpizza.com</p>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <FaInstagram />
+        <CiTwitter />
+        <RiTelegramLine />
+        <CiLinkedin />
+      </div>
+      <p>&copy: 2021pedrotechpizza.com</p>
+    </div>
+  );
 }
